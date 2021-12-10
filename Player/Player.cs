6fa7014d664dcf040351d56dd0db12823d9fc77b
Jason <429978+spaceyjase@users.noreply.github.com>
2021-12-10 14:39:50 +0000

    Small tweaks to the environment.

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public class Player : PathFollow
 {
@@ -10,14 +9,12 @@ public class Player : PathFollow
   [Export] private float yBounds = 2f;
 
   private Spatial player;
-  private Camera camera;
 
   public override void _Ready()
   {
     base._Ready();
     
     player = GetNode<Spatial>("Arwing");
-    camera = GetNode<Camera>("Camera");
   }
 
   private void SetSpeed(float speed, float delta)
@@ -32,13 +29,13 @@ public class Player : PathFollow
     var h = Input.GetAxis("left", "right");
     var v = Input.GetAxis("up", "down");
 
-    LocalMove(h, v, movementSpeed, delta);
-    Roll(player, v, h, 80f, .1f);
+    LocalMove(player, h, v, movementSpeed * delta);
+    Roll(player, v, h, 80f, .1f);   // TODO: [Export] values
 
     SetSpeed(forwardSpeed, delta);
   }
 
-  private void Roll(Spatial target, float v, float h, float leanLimit, float lerpTime)
+  private static void Roll(Spatial target, float v, float h, float leanLimit, float lerpTime)
   {
     target.Rotation = new Vector3(
       Mathf.LerpAngle(target.Rotation.x, v, lerpTime),
@@ -46,17 +43,17 @@ public class Player : PathFollow
       Mathf.LerpAngle(target.Rotation.z, -h * leanLimit, lerpTime));
   }
 
-  private void LocalMove(float x, float y, float speed, float delta)
+  private void LocalMove(Spatial target, float x, float y, float speed)
   {
-    player.Translation += new Vector3(-x, -y, 0) * speed * delta;
-    ClampPosition();
+    target.Translation += new Vector3(-x, -y, 0) * speed;
+    ClampPosition(target);
   }
 
-  private void ClampPosition()
+  private void ClampPosition(Spatial target)
   {
-    var translation = player.Translation;
+    var translation = target.Translation;
     translation.x = Mathf.Clamp(translation.x, -xBounds, xBounds);
     translation.y = Mathf.Clamp(translation.y, -yBounds, yBounds);
-    player.Translation = translation;
+    target.Translation = translation;
   }
 }
